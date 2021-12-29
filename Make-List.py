733a3ff6e9c9626ab7f50efc00ba2029e79b17d6
Hanialssadi <96384016+Hanialssadi@users.noreply.github.com>
2021-12-29 19:45:38 +0300

    Update Make-List.py

--- Make-List.py
+++ Make-List.py
@@ -3,9 +3,9 @@ print ("Coded by:\n=>VIGITA_MHS")
 def ViGiTa_MhS():
     password = open("vigitamhs.txt","w")
     print ("[+] - please wite")
-    i =300000000
-    while i<=399999999:
-        i+=1
+    i 49999999991
+    while 
+        i1999999999
         password.write (str(i))
         password.write ("\n")   
     password.close()
